14c35a023274b393fe9a1911d5f11f58219a114b
jlp717 <90418425+jlp717@users.noreply.github.com>
2022-03-17 13:34:42 +0100

    Update NbaStatsJUnit5.java
    Actualización al test

--- practica1/NbaStatsJUnit5.java
+++ practica1/NbaStatsJUnit5.java
@@ -14,17 +14,16 @@ public class NbaStatsJUnit5 {
 		System.out.println("***TOP 10 MEJORES JUGADORES CON ORDEN NORMAL***");
 		
 		ArrayList<Player> arrayADevolver;
-		String salidaEsperada = "[Player [Name = Wilt Chamberlain*| Score=785], "+
-				"Player [Name = DeMar DeRozan| Score=820], "+
-				"Player [Name = Damian Lillard| Score=825], "+
-				"Player [Name = Kevin Durant| Score=864], "+
-				"Player [Name = Anthony Davis| Score=894], "+
-				"Player [Name = Russell Westbrook| Score=931], "+
-				"Player [Name = Karl-Anthony Towns| Score=965], "+
-				"Player [Name = Stephen Curry| Score=975], "+
-				"Player [Name = James Harden| Score=994], "+
-				"Player [Name = LeBron James| Score=1031]"
-				+ "]";
+		String salidaEsperada = "[Player [Name = Wilt Chamberlain*| Score=785| Teams=[PHW, SFW, TOT, PHI, LAL]| Positions=[C], " +
+				"Player [Name = DeMar DeRozan| Score=820| Teams=[TOR]| Positions=[SG], " + 
+				"Player [Name = Damian Lillard| Score=825| Teams=[POR]| Positions=[PG], " +
+				"Player [Name = Kevin Durant| Score=864| Teams=[SEA, OKC, GSW]| Positions=[SG, SF], " +
+				"Player [Name = Anthony Davis| Score=894| Teams=[NOH, NOP]| Positions=[PF, C], " +
+				"Player [Name = Russell Westbrook| Score=931| Teams=[OKC]| Positions=[PG], " +
+				"Player [Name = Karl-Anthony Towns| Score=965| Teams=[MIN]| Positions=[C], " +
+				"Player [Name = Stephen Curry| Score=975| Teams=[GSW]| Positions=[PG], " +
+				"Player [Name = James Harden| Score=994| Teams=[OKC, HOU]| Positions=[SG, PG], " + 
+				"Player [Name = LeBron James| Score=1031| Teams=[CLE, MIA]| Positions=[SG, SF, PF]]";
 		
 		DyVSolution.cargarArchivo("C:\\WORKSPACES\\EDA_2022\\practica_1\\NbaStats.txt");
 		arrayADevolver = DyVSolution.diezMejoresJugadores();
@@ -39,17 +38,16 @@ public class NbaStatsJUnit5 {
 		System.out.println("\n***TOP 10 MEJORES JUGADORES CON REVERSE***");
 		
 		ArrayList<Player> arrayADevolver;
-		String salidaEsperada = "[Player [Name = LeBron James| Score=1031], " +
-				"Player [Name = James Harden| Score=994], " +
-				"Player [Name = Stephen Curry| Score=975], " +
-				"Player [Name = Karl-Anthony Towns| Score=965], " +
-				"Player [Name = Russell Westbrook| Score=931], " + 
-				"Player [Name = Anthony Davis| Score=894], " +
-				"Player [Name = Kevin Durant| Score=864], " +
-				"Player [Name = Damian Lillard| Score=825], " +
-				"Player [Name = DeMar DeRozan| Score=820], " + 
-				"Player [Name = Wilt Chamberlain*| Score=785]"
-				+ "]";
+		String salidaEsperada = "[Player [Name = LeBron James| Score=1031| Teams=[CLE, MIA]| Positions=[SG, SF, PF], " +
+				"Player [Name = James Harden| Score=994| Teams=[OKC, HOU]| Positions=[SG, PG], " +
+				"Player [Name = Stephen Curry| Score=975| Teams=[GSW]| Positions=[PG], " +
+				"Player [Name = Karl-Anthony Towns| Score=965| Teams=[MIN]| Positions=[C], " +
+				"Player [Name = Russell Westbrook| Score=931| Teams=[OKC]| Positions=[PG], " +
+				"Player [Name = Anthony Davis| Score=894| Teams=[NOH, NOP]| Positions=[PF, C], " +
+				"Player [Name = Kevin Durant| Score=864| Teams=[SEA, OKC, GSW]| Positions=[SG, SF], " +
+				"Player [Name = Damian Lillard| Score=825| Teams=[POR]| Positions=[PG], " + 
+				"Player [Name = DeMar DeRozan| Score=820| Teams=[TOR]| Positions=[SG], " +
+				"Player [Name = Wilt Chamberlain*| Score=785| Teams=[PHW, SFW, TOT, PHI, LAL]| Positions=[C]]";
 		
 		DyVSolution.cargarArchivo("C:\\WORKSPACES\\EDA_2022\\practica_1\\NbaStats.txt");
 		arrayADevolver = DyVSolution.diezMejoresJugadores();
