5d9bf39368a82425e7c571b55de783409ee387ec
jlp717 <90418425+jlp717@users.noreply.github.com>
2022-03-22 18:06:24 +0100

    Update DYV.java

--- practica1/sources/DYV.java
+++ practica1/sources/DYV.java
@@ -5,7 +5,7 @@ import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-import practica_1_Agustin.Player;
+import practica_1.Player;
 
 public class DYV {
     
